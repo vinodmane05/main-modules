8b315599b9a61cb2224b1fd88716e940631a2bfb
vinodmane05 <vinod.mane@neurealm.com>
2025-12-07 22:15:54 +0530

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "name" {
+  value = azurerm_resource_group.tf_resource_group.name
+  description = "name of resource"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "location" {
+  type        = string
+  description = "Location of resource group"
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "Name of resource group"
+  default     = ""
+}
